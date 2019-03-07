b61a59d069eccee08beff8db6349c78192cacc37
ktndrnl <33733121+ktndrnl@users.noreply.github.com>
2019-03-06 20:47:22 -0600

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager S;
+
+	[Header("Set in Inspector")]
+	// UI
+	public Text uiScoreText;
+	public Text uiBestScoreText;
+	public GameObject pickupPrefab;
+
+	[Header("Set Dynamically")]
+	public int score;
+	public int bestScore;
+
+	private void Awake()
+	{
+		if (S == null)
+		{
+			S = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Start()
+	{
+		if (!PlayerPrefs.HasKey("bestScore"))
+		{
+			PlayerPrefs.SetInt("bestScore", 0);
+		}
+
+		bestScore = PlayerPrefs.GetInt("bestScore");
+		UpdateGUI();
+
+		SpawnPickup();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+	public void UpdateScore(int points)
+	{
+		score += points;
+		if (score > bestScore)
+		{
+			bestScore = score;
+			PlayerPrefs.SetInt("bestScore", score);
+		}
+		UpdateGUI();
+	}
+
+	public void UpdateGUI()
+	{
+		uiScoreText.text = $"SCORE: {score}";
+		uiBestScoreText.text = $"BEST: {bestScore}";
+	}
+
+	public void SpawnPickup()
+	{
+		Instantiate(pickupPrefab, new Vector3(Random.Range(2, 49),
+			Random.Range(2, 49), 0), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/PlayerSegment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSegment : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Pickup"))
+		{
+			Destroy(other.gameObject);
+			GameManager.S.SpawnPickup();
+		}
+	}
+}
