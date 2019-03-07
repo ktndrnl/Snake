9da4ed4464431b8e1087b79cc2fad48f1527adf7
ktndrnl <33733121+ktndrnl@users.noreply.github.com>
2019-03-07 06:10:23 -0600

    Refactor
    More to do

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,10 +4,23 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
+	public static AudioManager S;
 	public Sound[] sounds;
 
 	private void Awake()
 	{
+		if (S == null)
+		{
+			S = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+
 		foreach (Sound s in sounds)
 		{
 			s.source = gameObject.AddComponent<AudioSource>();
